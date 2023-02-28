163f326e96ff9367da7aa228cb48029c9babd279
Alexandre-st <alexandre.saintprix@gmail.com>
2023-02-28 12:15:47 +0100

    Add hero part with mobile css design

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -7,25 +7,24 @@ const Header = () => {
   return (
     <AnimatePresence>
       <motion.header
-        className="navigation container"
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
       >
-        <img className="navigation-logo" src={easybankLogo} alt="Easybank" />
-        <nav className="navigation-desktop">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Blog</li>
-            <li>Carrers</li>
-          </ul>
-        </nav>
-        <button className="navigation-button">
-          Request Invite
-        </button>
-        <Menu />
+        <div className="navigation container">
+          <img className="navigation-logo" src={easybankLogo} alt="Easybank" />
+          <nav className="navigation-desktop">
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Blog</li>
+              <li>Carrers</li>
+            </ul>
+          </nav>
+          <button className="navigation-button">Request Invite</button>
+          <Menu />
+        </div>
       </motion.header>
     </AnimatePresence>
   );
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,9 +1,26 @@
+import mockups from "../assets/images/image-mockups.png";
+
 const Homepage = () => {
   return (
-    <main className="container">
-      <h1>Homepage</h1>
+    <main>
+      <section className="hero container-img">
+        <div className="hero-img">
+          <img src={mockups} alt="Application mockups" />
+        </div>
+        <div className="hero-content">
+          <h1 className="hero-content-title">
+            Next generation digital banking
+          </h1>
+          <p className="hero-content-text text">
+            Take your financial life online. Your Easybank account will be a
+            one-stop-shop for spending, saving, budgeting, investing, and much
+            more.
+          </p>
+          <button className="hero-content-button button">Request Invite</button>
+        </div>
+      </section>
     </main>
   );
-}
- 
+};
+
 export default Homepage;
