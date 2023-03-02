3c69b27ddb571c13edbd5d74ac2aa547da833d0a
Alexandre-st <alexandre.saintprix@gmail.com>
2023-03-02 22:57:20 +0100

    Add the next part without style, and without animation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,11 +7,6 @@ const Menu = () => {
 
   const [openMenu, setOpenMenu] = useState(false);
 
-  const variants = {
-    open: { opacity: 1, x: 0 },
-    closed: { opacity: 0, x: "-100%" }
-  };
-
   const toggleMenu = () => {
     setOpenMenu(prevMenu => !prevMenu);
   };
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,22 +1,83 @@
+import { AnimatePresence, motion } from "framer-motion";
+// Image for the description
+import Api from "../assets/images/icon-api.svg";
+import Budgeting from "../assets/images/icon-budgeting.svg";
+import Onboarding from "../assets/images/icon-onboarding.svg";
+import Banking from "../assets/images/icon-online.svg";
+// Image for the Hero
 import mockups from "../assets/images/image-mockups.png";
 
 const Homepage = () => {
+
+  const Description = (props) => {
+    return (
+      <div className="description-content-article">
+        <img className="description-content-article-image" src={props.image} alt="logo" />
+        <h3>{props.title}</h3>
+        <p className="text">{props.text}</p>
+      </div>
+    );
+  };
+
   return (
     <main>
-      <section className="hero container-img">
-        <div className="hero-img">
-          <img src={mockups} alt="Application mockups" />
+      <AnimatePresence>
+        <motion.section
+          className="hero container-img"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="hero-img">
+            <img src={mockups} alt="Application mockups" />
+          </div>
+          <div className="hero-content">
+            <h1 className="hero-content-title big-title">
+              Next generation digital banking
+            </h1>
+            <p className="hero-content-text text">
+              Take your financial life online. Your Easybank account will be a
+              one-stop-shop for spending, saving, budgeting, investing, and much
+              more.
+            </p>
+            <button className="hero-content-button button">
+              Request Invite
+            </button>
+          </div>
+        </motion.section>
+      </AnimatePresence>
+      {/* Why Choose Easybank */}
+      <section className="description">
+        <div className="container">
+          <div className="description-header">
+            <h2 className="mid-title">Why choose Easyban?</h2>
+            <p className="text">
+              We leverage Open Banking to turn your bank account into your
+              financial hub. Control your finances like never before.
+            </p>
+          </div>
         </div>
-        <div className="hero-content">
-          <h1 className="hero-content-title big-title">
-            Next generation digital banking
-          </h1>
-          <p className="hero-content-text text">
-            Take your financial life online. Your Easybank account will be a
-            one-stop-shop for spending, saving, budgeting, investing, and much
-            more.
-          </p>
-          <button className="hero-content-button button">Request Invite</button>
+        <div className="description-content">
+          <Description
+            image={Banking}
+            title="Online Banking"
+            text="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
+          />
+          <Description
+            image={Budgeting}
+            title="Simple Budgeting"
+            text="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
+          />
+          <Description
+            image={Onboarding}
+            title="Fast Onboarding"
+            text="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
+          />
+          <Description
+            image={Api}
+            title="Open API"
+            text="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
+          />
         </div>
       </section>
     </main>
