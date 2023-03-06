befac269a099e6906edc91dd7ae94454145ea7e9
Alexandre-st <alexandre.saintprix@gmail.com>
2023-03-06 19:51:09 +0100

    Add the description part and design for mobile and desktop

--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -14,10 +14,10 @@ const Homepage = () => {
       <div className="description-content-article">
         <img className="description-content-article-image" src={props.image} alt="logo" />
         <h3>{props.title}</h3>
-        <p className="text">{props.text}</p>
+        <p className="mid-text">{props.text}</p>
       </div>
     );
-  };
+  }; 
 
   return (
     <main>
@@ -48,36 +48,36 @@ const Homepage = () => {
       </AnimatePresence>
       {/* Why Choose Easybank */}
       <section className="description">
-        <div className="container">
+        <div className="container description-container">
           <div className="description-header">
-            <h2 className="mid-title">Why choose Easyban?</h2>
+            <h2 className="mid-title">Why choose Easybank?</h2>
             <p className="text">
               We leverage Open Banking to turn your bank account into your
               financial hub. Control your finances like never before.
             </p>
           </div>
-        </div>
-        <div className="description-content">
-          <Description
-            image={Banking}
-            title="Online Banking"
-            text="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
-          />
-          <Description
-            image={Budgeting}
-            title="Simple Budgeting"
-            text="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
-          />
-          <Description
-            image={Onboarding}
-            title="Fast Onboarding"
-            text="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
-          />
-          <Description
-            image={Api}
-            title="Open API"
-            text="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
-          />
+          <div className="description-content">
+            <Description
+              image={Banking}
+              title="Online Banking"
+              text="Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world."
+            />
+            <Description
+              image={Budgeting}
+              title="Simple Budgeting"
+              text="See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits."
+            />
+            <Description
+              image={Onboarding}
+              title="Fast Onboarding"
+              text="We don’t do branches. Open your account in minutes online and start taking control of your finances right away."
+            />
+            <Description
+              image={Api}
+              title="Open API"
+              text="Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier."
+            />
+          </div>
         </div>
       </section>
     </main>
